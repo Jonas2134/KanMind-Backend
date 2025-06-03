331895f0280ee427d64d2cf4c395abfaa118697a
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-06-03 16:28:21 +0200

    Save interim status

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import CustomLoginView, RegistrationView
 
 urlpatterns = [
+    path('', CustomLoginView, name='login'),
     path('login/', CustomLoginView, name='login'),
     path('registration/', RegistrationView, name='registration'),
 ]
