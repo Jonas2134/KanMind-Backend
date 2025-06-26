44944a2c017de2a8299a1e058892b6c022fcea7e
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-06-26 17:36:06 +0200

    optimize ticket serializers

--- ticket_app/api/serializers.py
+++ ticket_app/api/serializers.py
@@ -6,11 +6,33 @@ from auth_app.api.serializers import UserNestedSerializer
 from boards_app.models import Board
 
 
-class TicketBaseSerializer(serializers.ModelSerializer):
+class TicketReadUsersMixin(serializers.Serializer):
     assignee = UserNestedSerializer(read_only=True)
     reviewer = UserNestedSerializer(read_only=True)
-    comments_count = serializers.SerializerMethodField()
 
+
+class TicketWriteUsersMixin(serializers.Serializer):
+    assignee_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), source='assignee',
+        allow_null=True, required=False
+    )
+    reviewer_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), source='reviewer',
+        allow_null=True, required=False
+    )
+
+
+class CommentCountMixin(serializers.Serializer):
+    comments_count = serializers.SerializerMethodField()
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+
+class BoardIdMixin(serializers.Serializer):
+    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
+
+
+class TicketBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = [
@@ -19,45 +41,23 @@ class TicketBaseSerializer(serializers.ModelSerializer):
             'description',
             'status',
             'priority',
-            'assignee',
-            'reviewer',
             'due_date',
-            'comments_count',
         ]
-        read_only_fields = ['id', 'comments_count', 'assignee', 'reviewer']
+        read_only_fields = ['id']
 
     def get_comments_count(self, obj):
         return obj.comments.count()
 
 
-class TicketSerializer(TicketBaseSerializer):
-    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
+class TicketSerializer(TicketBaseSerializer, TicketReadUsersMixin, BoardIdMixin, CommentCountMixin):
     class Meta(TicketBaseSerializer.Meta):
         model = Ticket
-        fields = TicketBaseSerializer.Meta.fields + ['board']
-        read_only_fields = TicketBaseSerializer.Meta.read_only_fields
+        fields = TicketBaseSerializer.Meta.fields + ['assignee', 'reviewer', 'board', 'comments_count']
+        read_only_fields = TicketBaseSerializer.Meta.read_only_fields + ['assignee', 'reviewer']
 
 
-class TicketCreateSerializer(serializers.ModelSerializer):
-    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
-    assignee_id = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), source='assignee', allow_null=True, required=False
-    )
-    reviewer_id = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), source='reviewer', allow_null=True, required=False
-    )
-
+class TicketCreateSerializer(TicketBaseSerializer, TicketWriteUsersMixin, BoardIdMixin):
     class Meta:
         model = Ticket
-        fields = [
-            'id',
-            'board',
-            'title',
-            'description',
-            'status',
-            'priority',
-            'assignee_id',
-            'reviewer_id',
-            'due_date',
-        ]
-        read_only_fields = ['id']
+        fields = TicketBaseSerializer.Meta.fields + ['assignee_id', 'reviewer_id', 'board']
+        read_only_fields = TicketBaseSerializer.Meta.read_only_fields
