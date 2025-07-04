ae53f5dbf646e1db82eafe8da73536f4de4b1dac
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-07-04 19:29:17 +0200

    BoardListCreateView optimize again

--- boards_app/api/views.py
+++ boards_app/api/views.py
@@ -16,51 +16,53 @@ class BoardListCreateView(generics.ListCreateAPIView):
         user = self.request.user
         allowed_ids = Board.objects.filter(Q(owner=user) | Q(members=user)).values_list('id', flat=True)
         return Board.objects.filter(id__in=allowed_ids).distinct()
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return BoardCreateSerializer
+        return BoardListSerializer
 
     @handle_exceptions(action='retrieving boards')
     def list(self, request, *args, **kwargs):        
-        qs = self.get_queryset().annotate(
+        qs = self._annotated_queryset(self.get_queryset())
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @handle_exceptions(action='creating board')
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        self._validate_members(request.data.get('members', []))
+        board = serializer.save(owner=request.user)
+        qs = self._annotated_queryset(Board.objects.filter(pk=board.pk)).first()
+        output_serializer = BoardListSerializer(qs, context={'request': request})
+        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+
+    def _validate_members(self, member_ids):
+        if not isinstance(member_ids, list):
+            raise ValidationError({'members': 'Must be a list of user IDs.'})
+        existing = set(User.objects.filter(id__in=member_ids).values_list('id', flat=True))
+        missing = set(member_ids) - existing
+        if missing:
+            raise ValidationError({'members':
+                f'The following members do not exist: {sorted(missing)}'})
+        if self.request.user.id in member_ids:
+            raise ValidationError({'members': 'You cannot add yourself as a member.'})
+
+    def _annotated_queryset(self, qs):
+        return qs.annotate(
             member_count=Count('members', distinct=True),
             ticket_count=Count('tickets', distinct=True),
             tasks_to_do_count=Count('tickets', filter=Q(tickets__status='todo')),
             tasks_high_prio_count=Count('tickets', filter=Q(tickets__priority='high')),
         )
-        serializer = BoardListSerializer(qs, many=True, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def _validate_payload(self, data):
-        if not data.get('title'):
-            raise ValidationError({'title': 'This field is required.'})
-        members = data.get('members', [])
-        if not isinstance(members, list):
-            raise ValidationError({'members': 'Must be a list of user IDs.'})
-        existing = set(User.objects.filter(id__in=members).values_list('id', flat=True))
-        missing = set(members) - existing
-        if missing:
-            raise ValidationError({'members': f'These members do not exist: {sorted(missing)}'})
-        if self.request.user.id in members:
-            raise ValidationError({'members': 'You cannot add yourself as a member.'})
-        return members
-    
-    def _create_board(self, user, title, members):
-        board = Board.objects.create(owner=user, title=title)
-        if members:
-            board.members.set(members)
-        return board
 
-    @handle_exceptions(action='creating board')
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        members = self._validate_payload(data)
-        board = self._create_board(request.user, data['title'], members)
-        qs = (Board.objects.filter(pk=board.pk).annotate(
-            member_count=Count('members', distinct=True),
-            ticket_count=Count('tickets', distinct=True),
-            tasks_to_do_count=Count('tickets', filter=Q(tickets__status='todo')),
-            tasks_high_prio_count=Count('tickets', filter=Q(tickets__priority='high')),
-        ).first())
-        output_serializer = BoardListSerializer(qs, context={'request': request})
-        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+    def _error_response(self):
+        return Response(
+            {'detail': 'Internal server error.'},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
 class BoardDetailPatchDeleteView(generics.RetrieveUpdateDestroyAPIView):
